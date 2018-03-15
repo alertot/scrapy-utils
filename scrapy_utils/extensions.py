d53ec1a4b8e87adb1f38c6afce2104f3167d95a8
Claudio Salazar <csalazar@spect.cl>
2018-03-15 22:00:21 +0100

    Add new ItemStatsExtension

---
+++ scrapy_utils/extensions.py
@@ -0,0 +1,31 @@
+from scrapy import signals
+
+
+class ItemStatsExtension(object):
+    """ Register field stats about scraped items. """
+
+    def __init__(self, stats):
+        self.stats = stats
+        self.registry = set()
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        o = cls(crawler.stats)
+        crawler.signals.connect(o.item_scraped, signal=signals.item_scraped)
+
+        return o
+
+    def item_scraped(self, item, spider):
+        item_classname = item.__class__.__name__
+
+        for field in item.fields:
+            key = 'itemstats/{}/{}'.format(item_classname, field)
+
+            # Initialize stats to also register fields with 0 ocurrences
+            if item_classname not in self.registry:
+                self.stats.set_value(key, 0, spider=spider)
+            else:
+                if field in item:
+                    self.stats.inc_value(key, spider=spider)
+
+        self.registry.add(item_classname)
